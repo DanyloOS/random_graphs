fc4c50dd4e55f2052a5e9fef1297597ead9c93d9
Template Janitor <template.janitor@example.com>
2025-04-13 17:16:16 +0000

    Cleanup template and initialize repository

--- cmake/Hardening.cmake
+++ cmake/Hardening.cmake
@@ -1,7 +1,7 @@
 include(CheckCXXCompilerFlag)
 
 macro(
-  myproject_enable_hardening
+  random_graphs_enable_hardening
   target
   global
   ubsan_minimal_runtime)
--- src/sample_library/sample_library.cpp
+++ src/sample_library/sample_library.cpp
@@ -1,4 +1,4 @@
-#include <myproject/sample_library.hpp>
+#include <random_graphs/sample_library.hpp>
 
 int factorial(int input) noexcept
 {
